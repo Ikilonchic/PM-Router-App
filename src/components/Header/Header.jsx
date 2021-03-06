b9a9a96b5c1d3382b1aaafdbe97255d0119e39b2
Ikilonchic <43388441+Ikilonchic@users.noreply.github.com>
2021-03-06 23:40:03 +0200

    Add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import styles from './Header.module.scss';
+
+const propTypes = {
+    className: PropTypes.string,
+};
+
+const Header = (props) => {
+    return <header className={classNames(props.className, styles['container'])}>
+        <nav className={styles['container__inner']}>
+            <ul className={styles['container__menu']}>
+                {Array.from(props.children).map(elem => <li className={styles['container__element']}>{elem}</li>)}
+            </ul>
+        </nav>
+    </header>;
+};
+
+Header.propTypes = propTypes;
+
+export default Header;
